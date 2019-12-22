f922b9125ac24eb0e8e8fd5fe26816f9fe9dffbd
jons63 <jonasgarsten@gmail.com>
2019-12-22 15:07:03 +0100

    Updated lane detection

---
+++ lane_detection_functions.py
@@ -0,0 +1,67 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+def make_coordiantes(image, line_parameters):
+    slope, intercept = line_parameters
+    y1 = image.shape[0]
+    y2 = int(y1*(3./5))
+    x1 = int((y1 - intercept)/slope)
+    x2 = int((y2 - intercept)/slope)
+    return np.array([x1,y1,x2,y2])
+
+def average_slope_intercept(image, lines):
+    ret = []
+    left_fit = []
+    right_fit = []
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        parameters = np.polynomial.polynomial.Polynomial.fit((x1,x2), (y1,y2),1)
+        parameters = parameters.convert().coef
+        if len(parameters) == 2:
+            slope = parameters[1]
+            intercept = parameters[0]
+        if slope < 0:
+            left_fit.append((slope, intercept))
+        else:
+            right_fit.append((slope, intercept))
+    if(len(left_fit) != 0):
+        left_fit_average = np.average(left_fit,axis=0)
+        left_line = make_coordiantes(image, left_fit_average)
+        ret.append(left_line)
+    if(len(right_fit) != 0):
+        right_fit_average = np.average(right_fit,axis=0)
+        right_line = make_coordiantes(image, right_fit_average)
+        ret.append(right_line)
+    return np.array(ret)
+
+def canny(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(gray,(5,5),0)
+    canny = cv2.Canny(blur,50,150)
+    return canny
+
+def display_lines(image, lines):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for x1, y1, x2, y2 in lines:
+            cv2.line(line_image, (x1,y1), (x2,y2), (255,0,0), 10)
+    return line_image
+
+def region_of_interest(image):
+    height = image.shape[0]
+    polygons = np.array([[(100, height), (1200, height), (650, 300)]])
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask, polygons, 255)
+    masked_image = cv2.bitwise_and(image,mask)
+    return masked_image
+
+
+def overlay_lane_detection(lane_image):
+    canny_image = canny(lane_image)
+    cropped_image = region_of_interest(canny_image)
+    lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=10, maxLineGap=5)
+    averaged_lines = average_slope_intercept(lane_image, lines)
+    line_image = display_lines(lane_image, averaged_lines)
+    combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1 )
+    return combo_image
--- main_AD.py
+++ main_AD.py
@@ -17,27 +17,81 @@ import random
 import time
 import numpy as np
 import cv2
-from lane_main import *
+from lane_detection_functions import overlay_lane_detection
 
-IM_WIDTH = 640
-IM_HEIGHT = 480
+
+RGB_image = None
+SEG_image = None
+IM_WIDTH = 1280
+IM_HEIGHT = 720
+actor_list = []
+RGB_img_ready = False
+bounding_box_ready = False
+bounding_box = []
 
 
 def process_img(image):
+    global RGB_image, RGB_img_ready
     i = np.array(image.raw_data)
     i2 = i.reshape((IM_HEIGHT, IM_WIDTH, 4))
     i3 = i2[:, :, :3]
     # Do processing
-    result = get_lanes(i3)
-    cv2.imshow("", result)
-    cv2.waitKey(0)
-    return i3
+    result = overlay_lane_detection(i3)
+    cv2.imshow("Lane detection", result)
+    cv2.waitKey(250)
+    RGB_image = i3
+    RGB_img_ready = True
 
 
-actor_list = []
+def save_OD_screenshots(SEG_img):
+    global bounding_box, bounding_box_ready
+    i = np.array(SEG_img.raw_data)
+    i2 = i.reshape((IM_HEIGHT, IM_WIDTH, 4))
+    i3 = i2[:, :, 2:3]
+    # Find interesting area
+    list_of_pixel_coord_with_cars = np.where(i3 == 10)
+    #print(list_of_pixel_coord_with_cars)
+    coord = list(zip(list_of_pixel_coord_with_cars[0], list_of_pixel_coord_with_cars[1]))
+    #print(coord)
+    # iterate over the list of coordinates
+    x_min = np.min(list_of_pixel_coord_with_cars[1])
+    x_max = np.max(list_of_pixel_coord_with_cars[1])
+    y_min = np.max(list_of_pixel_coord_with_cars[0])
+    y_max = np.min(list_of_pixel_coord_with_cars[0])
+    #for cord in coord:
+    #    print(cord)
+    # Draw bounding box
+    bounding_box = [x_min, x_max, y_min, y_max]
+    bounding_box_ready = True
+
+
+def save_screenshot():
+    global RGB_image, bounding_box
+    result = cv2.rectangle(RGB_image, (bounding_box[0], bounding_box[2]), (bounding_box[1], bounding_box[3]), (0, 0, 255), 3, cv2.LINE_AA)
+    cv2.imshow("Object detection", result)
+    cv2.waitKey(250)
+    #print("SAVE SCREENSHOT FUNCTION")
+
+
+def add_test_car(spawn_point):
+    spawn_point_2 = spawn_point
+    if 10 > spawn_point.rotation.yaw > -10:
+        spawn_point_2.location.x = spawn_point_2.location.x + 8
+    elif 100 > spawn_point.rotation.yaw > 80:
+        spawn_point_2.location.y = spawn_point_2.location.y + 8
+    elif -80 > spawn_point.rotation.yaw > -100:
+        spawn_point_2.location.y = spawn_point_2.location.y - 8
+    else:
+        spawn_point_2.location.x = spawn_point_2.location.x - 8
+    print("Spawn point test car:", spawn_point_2)
+    vehicle2 = world.spawn_actor(bp, spawn_point_2)
+    vehicle2.set_autopilot(True)  # if you just wanted some NPCs to drive.
+    actor_list.append(vehicle2)
+
+
 try:
     client = carla.Client('localhost', 2000)
-    client.set_timeout(2.0)
+    client.set_timeout(5.0)
 
     world = client.get_world()
 
@@ -47,34 +101,52 @@ try:
     print(bp)
 
     spawn_point = random.choice(world.get_map().get_spawn_points())
+    print("Spawn point host:", spawn_point)
 
     vehicle = world.spawn_actor(bp, spawn_point)
-    # vehicle.apply_control(carla.VehicleControl(throttle=1.0, steer=0.0))
     vehicle.set_autopilot(True)  # if you just wanted some NPCs to drive.
-
     actor_list.append(vehicle)
+
+    add_test_car(spawn_point)
+
+    # vehicle.apply_control(carla.VehicleControl(throttle=1.0, steer=0.0))
+
 
     # https://carla.readthedocs.io/en/latest/cameras_and_sensors
     # get the blueprint for this sensor
-    blueprint = blueprint_library.find('sensor.camera.rgb')
+    blueprint_rgb = blueprint_library.find('sensor.camera.rgb')
     # change the dimensions of the image
-    blueprint.set_attribute('image_size_x', f'{IM_WIDTH}')
-    blueprint.set_attribute('image_size_y', f'{IM_HEIGHT}')
-    blueprint.set_attribute('fov', '110')
+    blueprint_rgb.set_attribute('image_size_x', f'{IM_WIDTH}')
+    blueprint_rgb.set_attribute('image_size_y', f'{IM_HEIGHT}')
+    blueprint_rgb.set_attribute('fov', '110')
+
+    # get the blueprint for this sensor
+    blueprint_seg = blueprint_library.find('sensor.camera.semantic_segmentation')
+    # change the dimensions of the image
+    blueprint_seg.set_attribute('image_size_x', f'{IM_WIDTH}')
+    blueprint_seg.set_attribute('image_size_y', f'{IM_HEIGHT}')
+    blueprint_seg.set_attribute('fov', '110')
 
     # Adjust sensor relative to vehicle
     spawn_point = carla.Transform(carla.Location(x=2.5, z=1.3), carla.Rotation(pitch=-0.5))
 
     # spawn the sensor and attach to vehicle.
-    sensor = world.spawn_actor(blueprint, spawn_point, attach_to=vehicle)
+    sensor_rgb = world.spawn_actor(blueprint_rgb, spawn_point, attach_to=vehicle)
+    # spawn the sensor and attach to vehicle.
+    sensor_seg = world.spawn_actor(blueprint_seg, spawn_point, attach_to=vehicle)
 
     # add sensor to list of actors
-    actor_list.append(sensor)
+    actor_list.append(sensor_rgb)
+    # add sensor to list of actors
+    actor_list.append(sensor_seg)
 
     # do something with this sensor
-    sensor.listen(lambda data: process_img(data))
+    sensor_rgb.listen(lambda data: process_img(data))
+    #sensor_seg.listen(lambda data: save_OD_screenshots(data))
     while True:
-        time.sleep(10) #make function to sleep for 10 seconds
+        #time.sleep(1) #make function to sleep for 10 seconds
+        if bounding_box_ready and RGB_img_ready:
+            save_screenshot()
 
 finally:
     print('destroying actors')
